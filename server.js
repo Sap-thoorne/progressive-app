847b44b4e41e6ba9b50a5dca0dea3ca5416a3b7a
Sap-Thoorne <sapthoorne@outlook.com>
2020-05-18 05:09:03 +0530

    again changes happened

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
-const io = require('socket.io')(http)
-const port = process.env.port || 3000
+const io = require('socket.io')(http);
+const port = process.env.port || 3000;
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public'));
 
 let clients = 0
 
