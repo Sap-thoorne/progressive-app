73384dec9a16d2a10920bcae0af2eb144c6333cb
Sap-Thoorne <sapthoorne@outlook.com>
2020-05-18 04:12:53 +0530

    ready for heroku again

---
+++ public/index.js
@@ -0,0 +1,76 @@
+let Peer = require('simple-peer')
+let socket = io()
+const video = document.querySelector('video')
+let client = {}
+
+// We try to obtain the video stream
+
+navigator.mediaDevices.getUserMedia = (navigator.mediaDevices.getUserMedia || navigator.mediaDevices.webkitGetUserMedia || navigator.mediaDevices.mozGetUserMedia || navigator.mediaDevices.msGetUserMedia);
+
+if(navigator.mediaDevices.getUserMedia){
+    navigator.mediaDevices.getUserMedia({video: true, audio: true}).then(stream => {
+        socket.emit('NewClient');
+        video.srcObject = stream;
+        video.play();
+
+        // This is to initiate the stream for media exchange.
+        function InitPeer(type){
+            let peer = new Peer({
+                initiator : (type == 'init') ? true : false,
+                stream: stream,
+                trickle: false
+            });
+            peer.on('stream', function(stream){CreateVideo(strem);});
+            peer.on('close', function(){
+               document.getElementById('peerVideo').remove();
+               peer.destroy();
+            });
+        }
+
+        //To send an offer, initiating peer takes up the role
+        function MakePeer(){
+            client.gotAnswer = false;
+            let peer = InitPeer('init');
+            peer.on('signal', function(data){
+                if(!client.gotAnswer){
+                    socket.emit('Offer', data);
+                }
+            });
+            client.peer = peer;
+        }
+
+        function FrontAnswer(offer){
+            let peer = InitPeer('notInit');
+            peer.on('signal', (data) => {socket.emit('Answer', data)});
+            peer.signal(offer);
+        }
+
+        function SignalAnswer(answer){
+            client.gotAnswer = true;
+            let peer = client.peer;
+            peer.signal(answer);
+        }
+
+        function CreateVideo(stream){
+            let video = document.createElement('video');
+            video.id = "peerVideo";
+            video.srcObject = stream;
+            video.class = 'embed-responsive-item';
+            document.querySelector('#peerDiv').appendChild(video);
+            video.play();
+        }
+
+        function SessionActive(){
+            document.write("Session Active. Please come back late");
+        }
+
+        socket.on('BackOffer', FrontAnswer);
+        socket.on('BackAnswer', SignalAnswer);
+        socket.on('SessionActive', SessionActive);
+        socket.on('CreatePeer', MakePeer);
+
+    }).catch(err => document.write(err));
+}
+else{
+    alert('Sorry, browser does not support getUserMedia');
+}
